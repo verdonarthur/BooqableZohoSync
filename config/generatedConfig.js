275501616275a67676f617977250e17c196c1181
Verdon Arthur <verdon.arthur@gmail.com>
2019-04-03 19:30:35 +0200

    little modif for heroku

--- config/generatedConfig.js
+++ config/generatedConfig.js
@@ -2,9 +2,9 @@ const config = require('./config')
 
 module.exports = {
     zoho_api_address: process.env.ZOHO_API_ADDRESS || config.zoho_api_address,
-    zoho_books_authtoken: process.env.ZOHO_AUTHTOKEN || "97d89d889a6423cca223a93d8d74c1f0",
-    zoho_organisation_id: process.env.ZOHO_ORGANISATION_ID || 20065941545,
-    booqable_api_key: process.env.BOOQABLE_API_KEY || "948ea62d2351268ef674fb9009d2d315",
-    booqable_api_address: process.env.BOOQABLE_API_ADDRESS || "https://dhf79728.booqable.com/api/1/",
-    url_database: process.env.MONGODB_URI || 'mongodb://localhost:27017/zobosync',
+    zoho_books_authtoken: process.env.ZOHO_AUTHTOKEN || config.zoho_books_authtoken,
+    zoho_organisation_id: process.env.ZOHO_ORGANISATION_ID || config.zoho_organisation_id,
+    booqable_api_key: process.env.BOOQABLE_API_KEY || config.booqable_api_key,
+    booqable_api_address: process.env.BOOQABLE_API_ADDRESS || config.booqable_api_address,
+    url_database: process.env.MONGODB_URI || config.url_database,
 }
