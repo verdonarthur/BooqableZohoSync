092f035e6f47c0e86f9d8a151452b24bdb0fd97d
Arthur Verdon <verdon.arthur@gmail.com>
2019-05-02 18:49:10 +0200

    Change cron to http for production

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV != 'production')
     new CronSyncManager('http://localhost', port)
 else
-    new CronSyncManager('https://localhost', port)
+    new CronSyncManager('http://localhost', port)
 
 
 
